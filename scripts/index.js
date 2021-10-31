94e308ba551cfb9c1e155d8e0d160f947bca7328
sensei <carlospunk41@gmail.com>
2021-10-31 16:01:49 +0100

    Refactor ;

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,3 @@
-import  { createVideo, displayVideos, appendVideo}   from './modules/video.js'
+import  { fetchVideos }   from './modules/video.js'
 
-var server = "http://localhost:3000/videos"
-
-fetch(server, {
-  "method": "GET"
-})
-.then(result => {
-  return result.json()
-})
-.then(data => {
-    displayVideos(data)
-  })
-.catch(err => {
-  console.error(err);
-});
+fetchVideos()
